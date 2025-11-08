3c4cc4010652fdd76f8e71505bfa56c7d6c8cb52
DominusX <nictfi@gmail.com>
2025-11-08 16:32:26 +0200

    perf(cleanup): streamline project for stability
    Remove problematic and unnecessary assets:
    
    - Remove FastScriptReload (causes crashes)
    
    - Remove Easy Build System (not needed)
    
    - Remove all example/demo folders
    
    - Remove unused shader packages (HDRP, LWRP, Built-In)
    
    - Keep only URP shaders (project uses URP)
    
    Impact: Reduced asset count, improved load times, better stability

--- Assets/Easy Build System/Packages/Addons/Advanced Buildings/Scripts/IInteractable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EasyBuildSystem.Packages.Addons.AdvancedBuilding
-{
-    public enum InteractableType { BUILDABLE, CARRIABLE }
-
-    public interface IInteractable
-    {
-        InteractableType InteractableType { get; }
-    }
-}
--- Assets/Easy Build System/Packages/Addons/Advanced Buildings/Scripts/UI/ResourceSlotUI.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class ResourceSlotUI : MonoBehaviour
-{
-    public Image Icon;
-    public Text Text;
-}
